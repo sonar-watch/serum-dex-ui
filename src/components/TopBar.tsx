c9f503762561203762612ac0254acfbccabf2663
Baurain Olivier <baurain.o@gmail.com>
2021-05-21 10:16:11 +0200

    fix(topbar): remove add market link

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -185,11 +185,6 @@ export default function TopBar() {
               CONVERT
             </Menu.Item>
           )}
-          {(!searchFocussed || location.pathname === '/list-new-market') && (
-            <Menu.Item key="/list-new-market" style={{ margin: '0 10px' }}>
-              ADD MARKET
-            </Menu.Item>
-          )}
         </Menu>
         <div
           style={{
