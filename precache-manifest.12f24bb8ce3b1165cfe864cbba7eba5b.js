eea8f0a0c10172e03642f1f6bc6de51379e2a2d7
Baurain Olivier <baurain.o@gmail.com>
2021-07-11 10:22:07 +0200

    Updates

---
+++ precache-manifest.12f24bb8ce3b1165cfe864cbba7eba5b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b49cfcbaff4b3055a1c5426cc5f0f52",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b5c9cd181313a6a463f1",
+    "url": "/static/css/2.06ebec9a.chunk.css"
+  },
+  {
+    "revision": "a92539e61f70635c0c24",
+    "url": "/static/css/main.ce0eaf9a.chunk.css"
+  },
+  {
+    "revision": "b5c9cd181313a6a463f1",
+    "url": "/static/js/2.21997b32.chunk.js"
+  },
+  {
+    "revision": "c84ff2ee1e84300b54bc9f2005e87aef",
+    "url": "/static/js/2.21997b32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a92539e61f70635c0c24",
+    "url": "/static/js/main.fe89acf3.chunk.js"
+  },
+  {
+    "revision": "d3ebb56a3098223b5316",
+    "url": "/static/js/runtime-main.c7319a82.js"
+  },
+  {
+    "revision": "4665a36b158338b8cf0e787a3315b02a",
+    "url": "/static/media/logo.4665a36b.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b9cfd5491cf84f352b698d37a6f1e377.js"
+  "/precache-manifest.12f24bb8ce3b1165cfe864cbba7eba5b.js"
 );
 
 self.addEventListener('message', (event) => {
